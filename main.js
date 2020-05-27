8ec7a8039f80442a77836e0b4d73290869d25e34
anshqhsh <60730765+anshqhsh@users.noreply.github.com>
2020-05-27 22:41:14 +0900

    JS/navbarmenuClick

--- main.js
+++ main.js
@@ -11,3 +11,17 @@ document.addEventListener("scroll", () => {
     navbar.classList.remove(`navbar--dark`);
   }
 });
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector(`.navbar__menu`);
+navbarMenu.addEventListener(`click`, (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  //link가 있는 경우에만 navbar 기능 수행
+  console.log(event.target.dataset.link);
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+});
