e79cd29a44032de8923abd05799643ed8230f943
LJH's MacBookPro16 <joonhyuklee@MacBook-Pro-2.local>
2021-01-26 12:42:55 +0900

    selectErr
    ㅜㅜ

--- main.js
+++ main.js
@@ -1,109 +1,151 @@
-"use strict";
+'use strict';
 
-//Make navbar transparent when it is on the top
-
-const navbar = document.querySelector("#navbar");
+// Make navbar transparent when it is on the top
+const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
-document.addEventListener("scroll", () => {
+document.addEventListener('scroll', () => {
   if (window.scrollY > navbarHeight) {
-    navbar.classList.add(`navbar--dark`);
+    navbar.classList.add('navbar--dark');
   } else {
-    navbar.classList.remove(`navbar--dark`);
+    navbar.classList.remove('navbar--dark');
   }
 });
 
 // Handle scrolling when tapping on the navbar menu
-const navbarMenu = document.querySelector(`.navbar__menu`);
-navbarMenu.addEventListener(`click`, (event) => {
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', event => {
   const target = event.target;
   const link = target.dataset.link;
   if (link == null) {
     return;
   }
-  //link가 있는 경우에만 navbar 기능 수행
-  //   console.log(event.target.dataset.link);
-  navbarMenu.classList.remove(`open`);
+  navbarMenu.classList.remove('open');
   scrollIntoView(link);
 });
 
 // Navbar toggle button for small screen
-const navbarToggleBtn = document.querySelector(`.navbar__toggle-btn`);
-navbarToggleBtn.addEventListener("click", () => {
-  navbarMenu.classList.toggle("open");
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
 });
 
-//Handle click on "contact me" button on home
-const homeContactBtn = document.querySelector(`.home__contact`);
-homeContactBtn.addEventListener(`click`, () => {
-  scrollIntoView(`#contact`);
+// Handle click on "contact me" button on home
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', () => {
+  scrollIntoView('#contact');
 });
 
 // Make home slowly fade to transparent as the window scrolls down
-const home = document.querySelector(`.home__container`);
+const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
-document.addEventListener(`scroll`, () => {
+document.addEventListener('scroll', () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
-  //   console.log(1 - window.scrollY / homeHeight);
 });
 
 // Show "arrow up" button when scrolling down
-const arrowUp = document.querySelector(`.arrow-up`);
-document.addEventListener(`scroll`, () => {
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
   if (window.scrollY > homeHeight / 2) {
-    arrowUp.classList.add(`visible`);
+    arrowUp.classList.add('visible');
   } else {
-    arrowUp.classList.remove(`visible`);
+    arrowUp.classList.remove('visible');
   }
 });
-//Handle click on the "arrow up botton"
-arrowUp.addEventListener(`click`, () => {
-  scrollIntoView(`#home`);
+
+// Handle click on the "arrow up" button
+arrowUp.addEventListener('click', () => {
+  scrollIntoView('#home');
 });
 
-// Project
-const workBtnContainer = document.querySelector(`.work__categories`);
-const projectContainer = document.querySelector(`.work__projects`);
-const projects = document.querySelectorAll(`.project`);
-workBtnContainer.addEventListener("click", (e) => {
+// Projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', e => {
   const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
   if (filter == null) {
     return;
   }
 
-  //   Remove selection from the previous item and select the new one
-  const active = document.querySelector(`.category__btn.selected`);
+  // Remove selection from the previous item and select the new one
+  const active = document.querySelector('.category__btn.selected');
   if (active != null) {
-    active.classList.remove(`selected`);
+    active.classList.remove('selected');
   }
+  e.target.classList.add('selected');
 
-  e.target.classList.add(`selected`);
-
-  projectContainer.classList.add(`anim-out`);
-
+  projectContainer.classList.add('anim-out');
   setTimeout(() => {
-    projects.forEach((project) => {
+    projects.forEach(project => {
       console.log(project.dataset.type);
-      if (filter === `*` || filter === project.dataset.type) {
-        project.classList.remove(`invisible`);
+      if (filter === '*' || filter === project.dataset.type) {
+        project.classList.remove('invisible');
       } else {
-        project.classList.add(`invisible`);
+        project.classList.add('invisible');
       }
     });
-    projectContainer.classList.remove(`anim-out`);
+    projectContainer.classList.remove('anim-out');
   }, 300);
-  //   console.log(`+++++++++++0-----`);
-  //   for (let project of projects) {
-  //     console.log(project);
-  //   }
-  //   console.log(`+++++++++++`);
-  //   let project;
-  //   for (let i = 0; i < projects.length; i++) {
-  //     project = projects[i];
-  //     console.log(project);
-  //   } 모두 동일
 });
 
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
-  scrollTo.scrollIntoView({ behavior: `smooth` });
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
 }
+
+// 1. 모든 섹션 요소들과 메뉴아이템들을 가지고 온다
+// 2. IntersectionObserver를 이용해서 모든 섹션들을 관찰한다
+// 3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다
+const sectionIds = [
+  '#home',
+  '#about',
+  '#skills',
+  '#work',
+  '#education',
+  '#contact',
+];
+const sections = sectionIds.map(id => document.querySelector(id));
+const navItems = sectionIds.map(id =>
+  document.querySelector(`[data-link="${id}"]`)
+);
+//현재 선택되어 있는 내비게이션 아이디를 기억하고 있다가 다음선택이 될때 전의 것의 액티브 지우고 지금것에 active적용해줘야함 
+let selectedNavIndex = 0; //인덱스
+let selectedNavItem = navItems[0];//navItem 배열의 selectedNavIndex
+function selectNavItem(selected) {
+  selectedNavItem.classList.remove('active');//액티브를 삭제
+  selectedNavItem = selected;
+  selectedNavItem.classList.add('active');
+}
+
+const observerOptions = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.3,
+};
+
+const observerCallback = (entries, observer) => {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting && entry.intersectionRatio > 0) {
+      const index = sectionIds.indexOf(`#${entry.target.id}`);
+      console.log(index, entry.target.id); //인덱스, 타겟 id 출력
+      // y<0이면 스크롤링이 아래로 되어서 페이지가 올라옴 , 방향 
+      if (entry.boundingClientRect.y < 0) {
+        selectedNavIndex = index + 1;
+      } else {
+        selectedNavIndex = index - 1;
+      }//페이지가 내려가는 경우에는(y>0) 이전 인덱스로 지정
+    }
+  });
+};
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+sections.forEach(section => observer.observe(section));
+
+window.addEventListener('scroll', ()=> {
+  if(window.scrollY === 0){
+    selectedNavIndex = 0;
+  }else if(window.scrollY + window.innerHeight == document.body.clientHeight){//스크롤 끝에 도달
+    seledctedNavIndex = navItems.length - 1;
+  }
+  selectedNavItem(navItems[selectedNavIndex]);
+})
